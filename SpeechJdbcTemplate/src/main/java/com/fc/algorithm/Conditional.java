e3f553a7527684bed0bc907e0343689f79631e6b
Tom <1485217094@qq.com>
2021-08-04 17:08:40 +0800

    di

--- SpeechJdbcTemplate/src/main/java/com/fc/algorithm/Conditional.java
+++ SpeechJdbcTemplate/src/main/java/com/fc/algorithm/Conditional.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 //利用条件运算符的嵌套来完成此题：学习成绩> =90分的同学用A表示
 // 60-89分之间的用B表示，60分以下的用C表示
 public class Conditional {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("输入学生成绩:");
